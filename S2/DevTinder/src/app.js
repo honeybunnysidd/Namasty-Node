4be400e2a16eac8a0037dbbaa459cdcb3ab3c773
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-05 21:22:14 +0530

    create server

---
+++ S2/DevTinder/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Server is working!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening");
+});
