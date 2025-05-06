bab6791ba9aaf88a61985f9855ff0f4fca491e7d
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-06 08:58:30 +0530

    created a server

--- S2/DevTinder/src/app.js
+++ S2/DevTinder/src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ fName: "Siddhartha", lName: "Raghuvanshi" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the DB");
+});
+
 app.use("/hello", (req, res) => {
   res.send("Server is working!");
 });
+
+// app.use("/", (req, res) => {
+//   res.send("I am all path");
+// });
 
 app.listen(3000, () => {
   console.log("Server is listening");
