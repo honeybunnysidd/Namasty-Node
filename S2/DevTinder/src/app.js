e16ebce93015ae4ca1731aa21425ad5fe9d5e160
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-06-24 00:21:10 +0530

    DB Connection

--- S2/DevTinder/src/app.js
+++ S2/DevTinder/src/app.js
@@ -1,34 +1,17 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("../middlewares/auth.js");
+const { connectDB } = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res, next) => {
-  res.send("This is your data");
-  next();
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted the data");
-});
-
-//User Auth
-app.get("/user/login", (req, res) => {
-  res.send("You are logged in");
-});
-
-app.get("/user/profile", userAuth, (req, res) => {
-  res.send("This is your profile");
-});
-
-app.post("/user/update", (req, res) => {
-  res.send("req post of profile");
-});
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-
-//Incedo
+//DataBase Connection
+connectDB()
+  .then(() => {
+    console.log("DB connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Error : ", err);
+  });
---
+++ S2/DevTinder/src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  await mongoose.connect(
+    "mongodb+srv://honeybunnysidd:Siddhartha1027%40@cluster0.ep5jryt.mongodb.net/devTinder"
+  );
+}
+
+module.exports = { connectDB };
