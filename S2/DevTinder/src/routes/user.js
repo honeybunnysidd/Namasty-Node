f6537add71e9124a78ac6e89300696379adb41d0
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-07-03 03:52:39 +0530

    fix bug

--- S2/DevTinder/src/routes/user.js
+++ S2/DevTinder/src/routes/user.js
@@ -5,6 +5,8 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
+const USER_POPULATE_DATA = "firstName lastName age gender about skills";
+
 //Get all the pending request
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -13,14 +15,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "about",
-      "skills",
-      "gender",
-    ]);
+    }).populate("fromUserId", USER_POPULATE_DATA);
 
     if (connectionRequest.length < 1) {
       return res.send("There are no such peding requests");
@@ -45,24 +40,22 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedUser._id, status: "accepted" },
         { fromUserId: loggedUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "about",
-      "skills",
-      "gender",
-    ]);
-
-    const data = connectionRequest.map((el) => el.fromUserId);
+    })
+      .populate("fromUserId", USER_POPULATE_DATA)
+      .populate("toUserId", USER_POPULATE_DATA);
 
     if (connectionRequest.length < 1) {
       return res.send("There are no connection");
     }
-
-    res.json({
-      data,
+    const data = connectionRequest.map((el) => {
+      if (el.fromUserId._id.toString() === loggedUser._id.toString()) {
+        return el.toUserId;
+      } else {
+        return el.fromUserId;
+      }
     });
+
+    res.json({ data });
   } catch (err) {
     res.status(400).send("Error : " + err.message);
   }
