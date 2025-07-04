7d67b422cb634073ca527a75b73bb93c31b24088
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-07-05 02:56:58 +0530

    Setup Tailwind with Vite Manually

--- S2/DevTinder/Frontend/src/App.jsx
+++ S2/DevTinder/Frontend/src/App.jsx
@@ -1,11 +1,8 @@
 function App() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind CSS is working!
-      </h1>
-      <p className="mt-4 text-lg text-gray-700">No `npx` needed 😎</p>
-    </div>
+    <>
+      <h1>Tailwind CSS is working!</h1>;
+    </>
   );
 }
 
