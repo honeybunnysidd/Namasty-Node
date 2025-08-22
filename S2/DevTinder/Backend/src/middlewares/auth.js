86772bc19f41aa6bb3dfa82e07835c6535f3a0e3
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-08-22 09:09:24 +0530

    fix bug

--- S2/DevTinder/Backend/src/middlewares/auth.js
+++ S2/DevTinder/Backend/src/middlewares/auth.js
@@ -2,28 +2,29 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
-  try {
-    //Read the token from the req cookies
-    const { token } = req.cookies;
+	try {
+		//Read the token from the req cookies
+		const { token } = req.cookies;
 
-    if (!token) {
-      throw new Error("Please login");
-    }
-    //Validate token
-    const decodedObj = await jwt.verify(token, "secretCode");
+		if (!token) {
+			return res.status(401).send("Please login!");
+			// throw new Error("Please login");
+		}
+		//Validate token
+		const decodedObj = await jwt.verify(token, "secretCode");
 
-    const { _id } = decodedObj;
-    const user = await User.findById(_id);
+		const { _id } = decodedObj;
+		const user = await User.findById(_id);
 
-    if (!user) {
-      res.status(401).send("User does not exist");
-    } else {
-      req.user = user;
-      next();
-    }
-  } catch (err) {
-    res.status(400).send("Error: " + err.message);
-  }
+		if (!user) {
+			res.status(401).send("User does not exist");
+		} else {
+			req.user = user;
+			next();
+		}
+	} catch (err) {
+		res.status(400).send("Error: " + err.message);
+	}
 };
 
 module.exports = { userAuth };
